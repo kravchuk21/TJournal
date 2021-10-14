ec87ec01953ea46559ff7fb8ce98822a5f9122cf
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2021-10-14 22:36:16 +0300

    Верстка: форма добавления поста, форма добавления комментария, скрытие популярных комментариев

---
+++ pages/news/[slug].tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {MainLayout} from '../../layouts/MainLayout'
+import {FullPost} from '../../components/FullPost'
+import {PostComments} from '../../components/PostComments'
+
+export default function Post() {
+    return (
+        <MainLayout className="mb-50" contentFullWidth>
+            <FullPost/>
+            <PostComments/>
+        </MainLayout>
+    )
+}
